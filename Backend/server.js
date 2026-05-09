0631a04dc5b40c855a60f1be16e6a1c9ee6897a8
sanskrati1111 <sanskrati123arya@gmail.com>
2026-05-09 23:58:01 +0530

    adding interview code

--- Backend/server.js
+++ Backend/server.js
@@ -2,15 +2,15 @@ const app = require('./src/app');
 require("dotenv").config();
 const connectDB= require('./src/config/database');
 
-
 (async () => {
- 
-  
+  try {
+    await connectDB();
+    console.log('Connected to MongoDB');
 
-  await connectDB();
-
-
-  app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  } catch (error) {
+    console.error('Error starting server:', error.message);
+  }
 })();
